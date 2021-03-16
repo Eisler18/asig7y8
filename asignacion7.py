0cef68e0c889d924a8f4ce32cc71d3795d07d723
Eisler Aguilar <eislerandres1@gmail.com>
2021-03-16 12:13:07 -0400

    Modificando asignacion7.py para obtener tabla de dispositivos

--- asignacion7.py
+++ asignacion7.py
@@ -5,4 +5,21 @@ response = requests.post(
     'https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token',
     headers={'Authorization':'Basic ZGV2bmV0dXNlcjpDaXNjbzEyMyE='})
 payload=response.json()
-pprint(payload)
+#pprint(payload)
+
+Token = payload['Token']
+response = requests.get('https://sandboxdnac.cisco.com/dna/intent/api/v1/network-device', headers={'X-Auth-Token': Token})
+data = response.json()['response']
+#print(data)
+
+Table = [['family', 'hostname', 'managementIpAddress', 'lastUpdated', 'reachabilityStatus']]
+for device in data:
+    family = device['family']
+    hostname = device['hostname']
+    managementIpAddress = device['managementIpAddress']
+    lastUpdated = device['lastUpdated']
+    reachabilityStatus = device['reachabilityStatus']
+    Table.append([family, hostname, managementIpAddress, lastUpdated, reachabilityStatus])
+
+for row in Table:
+    print(row)
