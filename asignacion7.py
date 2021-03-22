6ca1617b604aefa16e900846dafbbc8a8f529e9f
Eisler Aguilar <eislerandres1@gmail.com>
2021-03-22 19:45:10 -0400

    Modificacion del archivo de Python para crear status.json

--- asignacion7.py
+++ asignacion7.py
@@ -1,26 +1,35 @@
 import requests
-from pprint import pprint
+import os
+import time
+import json
+
+FILE_NAME = 'status.json'
+FILE_DIR = r'C:\Users\Eisler Aguilar\Documents\USB\Electronica\EC5756 - Redes definidas por software\Tareas\Tarea 8\asig7y8'
+os.chdir(FILE_DIR)
 
 response = requests.post(
     'https://sandboxdnac.cisco.com/dna/system/api/v1/auth/token',
     headers={'Authorization':'Basic ZGV2bmV0dXNlcjpDaXNjbzEyMyE='})
 response.raise_for_status()
 payload=response.json()
-#pprint(payload)
+last_time = 0
 
-Token = payload['Token']
-response = requests.get('https://sandboxdnac.cisco.com/dna/intent/api/v1/network-device', headers={'X-Auth-Token': Token})
-data = response.json()['response']
-#print(data)
+while(True):
+    while(time.time()-last_time >= 5*60):
+        Token = payload['Token']
+        response = requests.get('https://sandboxdnac.cisco.com/dna/intent/api/v1/network-device', headers={'X-Auth-Token': Token})
+        data = response.json()['response']
+        
+        new_dict = {}
+        i = 1
+        for device in data:
+            new_dict.update({'device '+str(i): {'hostname': device['hostname'], 'status': device['reachabilityStatus']}})
+            i += 1
 
-Table = [['family', 'hostname', 'managementIpAddress', 'lastUpdated', 'reachabilityStatus']]
-for device in data:
-    family = device['family']
-    hostname = device['hostname']
-    managementIpAddress = device['managementIpAddress']
-    lastUpdated = device['lastUpdated']
-    reachabilityStatus = device['reachabilityStatus']
-    Table.append([family, hostname, managementIpAddress, lastUpdated, reachabilityStatus])
-
-for row in Table:
-    print(row)
+        with open(FILE_NAME, 'w') as f:
+            json.dump(new_dict, f, indent=2)
+        
+        if FILE_NAME in os.listdir("."):
+            print("\nEl archivo "+os.getcwd()+"/"+FILE_NAME+" ha sido actualizado")
+    
+        last_time = time.time()
